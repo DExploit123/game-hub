d9e1b334f5e01af238ccb0127a76ba8c0ef2ea9e
Daniel <onasanyadaniel794@gmail.com>
2025-11-06 14:02:51 +0100

    Ship genres with the app

--- src/components/Emoji.tsx
+++ src/components/Emoji.tsx
@@ -1,5 +1,5 @@
 import bullsEye from "../assets/bulls-eye.webp";
-import thumbsUp from "../assets/thumbsUp.webp";
+import thumbsUp from "../assets/thumbs-up.webp";
 import meh from "../assets/meh.webp";
 import { Image, ImageProps } from "@chakra-ui/react";
 interface Props {
